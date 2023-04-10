19db3d8aeff6eade6e71697afc40c6435ce31a3a
ywang552 <ywang552@syr.edu>
2023-04-10 19:25:50 -0400

    gogo

--- parameters.jl
+++ parameters.jl
@@ -2,13 +2,14 @@ DIM = 128
 TIMING = 37
 EpochNum_p = 1000
 ParentSize_p = 64
-γ_p = 256
+γ_p = 1024
 PB = 0.095
 λ_ = Int(γ_p*2)
 sms=0.1
-window_size = 16
-
-
+window_size_l = 128
+window_size_r = DIM
+is_ = 0
+js_ = 0 
 ## TO STORE 
 4867*0.0023
 0.0023
