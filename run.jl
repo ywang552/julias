d60edce7912da9d94933cf8a9ffd6c07c3f02ef1
Youchuan Wang <ywang552@syr.edu>
2023-04-19 22:17:52 -0400

    man

--- run.jl
+++ run.jl
@@ -1,44 +1,10 @@
 # ES_sliding(is = is_, js = js_, EpochNum= EpochNum_p, λ = λ_, DATASET = data_test, DIM_ = DIM, PB_ = PB, starting_mutation_strength = sms, windsize_l = window_size_l, windsize_r = window_size_l)
-# trialNum = 10
-# a = zeros(trialNum, EpochNum_p)
+trialNum = 30
+# a = Array{Ind}(undef, trialNum, μ_)
+
 
 # for i in 1:trialNum
-#     a[i, :] = ES_cheat()[2]
+#     println("------trialNum_", i,"-------")
+#     a[i,:]= ES_cheat()
 # end 
-
-
-plot(1:EpochNum_p,a[1,:])
-plot!(1:EpochNum_p,a[2,:])
-plot!(1:EpochNum_p,a[3,:])
-plot!(1:EpochNum_p,a[4,:])
-plot!(1:EpochNum_p,a[5,:])
-plot!(1:EpochNum_p,a[6,:])
-plot!(1:EpochNum_p,a[7,:])
-plot!(1:EpochNum_p,a[8,:])
-plot!(1:EpochNum_p,a[9,:])
-plot!(1:EpochNum_p,a[10,:])
-
-
-
-
-
-
-
-# a = perf
-# b = perf_
-# c = conv
-# d = gdn
-
-# a_ = perf
-# b_ = perf_
-# c_ = conv
-# d_ = gdn
-# b
-
-
-# a_[1:end, end]
-
-
-# plot(1:150, a_[1:end, end])
-# plot!(1:150, b)
 
