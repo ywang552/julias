3996f31bfac85db7e393db5a32a1e39b83c79ff0
Youchuan Wang <ywang552@syr.edu>
2023-04-20 16:06:25 -0400

    1

--- run.jl
+++ run.jl
@@ -1,5 +1,4 @@
 # ES_sliding(is = is_, js = js_, EpochNum= EpochNum_p, λ = λ_, DATASET = data_test, DIM_ = DIM, PB_ = PB, starting_mutation_strength = sms, windsize_l = window_size_l, windsize_r = window_size_l)
-trialNum = 30
 # a = Array{Ind}(undef, trialNum, μ_)
 
 
